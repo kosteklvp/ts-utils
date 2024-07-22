0d8ced4053b3e1233a37784e7387412c5f761946
kosteklvp <kostek.lvp@gmail.com>
2024-07-22 20:39:09 +0200

    update memoize.ts and memoize.test.ts

--- tests/decorators/memoize.test.ts
+++ tests/decorators/memoize.test.ts
@@ -3,15 +3,15 @@ import { memoize } from '../../src/decorators/memoize';
 describe('@Memoize()', () => {
     class MemoizeTest {
         @memoize()
-        static method(value: number) {
-            return value * 3;
+        static method() {
+            return new MemoizeTest();
         }
     }
     it('should cache the result of a static method', () => {
         const spy = jest.spyOn(MemoizeTest, 'method');
-        MemoizeTest.method(2);
-        MemoizeTest.method(2);
-        MemoizeTest.method(3);
-        expect(spy).toHaveBeenCalledTimes(2); // Called once for each unique set of arguments
+        MemoizeTest.method();
+        MemoizeTest.method();
+        MemoizeTest.method();
+        expect(spy).toHaveBeenCalledTimes(1); // Called once for each unique set of arguments
     });
 });
